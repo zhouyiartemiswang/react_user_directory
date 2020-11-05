197a726aa92c6929d21e0e57e4797460b2eef79f
Artemis <wzyzhouyi@gmail.com>
2020-11-04 21:00:31 -0800

    added jumbotron

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import React, { Component } from 'react'
+import Jumbotron from './components/Jumbotron'
 
 export default class App extends Component {
     render() {
         return (
-            <div>
+            // Jumbotron
+            <Jumbotron />
+            // Sort
+            // <div className="row">
+            //     <div className="col-md-6">
+
+            //     </div>
+            // </div>
+            // Search
+            // Table
+            // <div>
                 
-            </div>
+            // </div>
         )
     }
 }
---
+++ src/components/Jumbotron/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function Jumbotron() {
+    return (
+        <div class="jumbotron jumbotron-fluid">
+            <div class="container">
+                <h1 class="display-4">User Directory</h1>
+                <p class="lead">Use search box to narrow your results or sort by categories.</p>
+            </div>
+        </div>
+    )
+}
